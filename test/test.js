467895d2cf31615bf59651962802196ee86eaf9c
Ingo Mueller <github.com@ingomueller.net>
2015-11-20 15:12:00 +0000

    Change unit test to start server directly.
    If we start it through server.js, then everything that runs in
    the new process will not be tracked by the code coverage tool.

--- test/test.js
+++ test/test.js
@@ -1,14 +1,36 @@
 var expect = require('chai').expect;
 var request = require('superagent');
 
+var core = require('../core');
+var restify = require('restify');
+
+var userPath = '/users';
+var testPort = 1234;
+
 describe('OMS core server', function() {
 
-  describe('server serves content at /users', function() {
-    var url = 'http://localhost:8080/users';
+  beforeEach(function(done) {
+    server = restify.createServer();
+    server.get({path: userPath, version: '0.0.8'} , core.findAllUsers);
+    server.listen(testPort);
+    setTimeout(done, 10);
+  });
+
+  afterEach(function(done) {
+    setTimeout(function() {
+      server.close();
+      done();
+    }, 10);
+  });
+
+  describe('serves content at ' + userPath, function() {
+    var url = 'http://localhost:' + testPort + userPath;
 
     it('returns status 200', function(done) {
       request.get(url).end(function(err, res) {
+        /*jshint -W030 */
         expect(res).to.exist;
+        /*jshint +W030 */
         expect(res.status).to.equal(200);
         done();
       });
